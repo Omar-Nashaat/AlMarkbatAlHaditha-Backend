e73369eb4e294d46c45609f408d9537df7d02023
Omar-Nashaat <omarnashaat18@gmail.com>
2025-08-02 19:42:30 +0300

    change port

--- server.js
+++ server.js
@@ -63,5 +63,5 @@ app.use('/api/orders', orderRoutes)
 app.use('/api/admin', userRoutes)
 app.use('/api/offers', offerRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
